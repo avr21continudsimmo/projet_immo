49de1ba8e4614734d7f6877a174af0f8b12ec468
Benjamin LEBLANC <leblanc-benjamin@laposte.net>
2021-07-09 17:28:12 +0200

    DVF Filtrée
    avec filtres différent Paris et hors Paris

--- dvf_filters.py
+++ dvf_filters.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+#import numpy as np
 #import seaborn as sns
 #import matplotlib.pyplot as plt
 
@@ -11,9 +11,9 @@ communes_littorales = pd.read_csv("communes_littorales_2019.csv", sep=";")
 ##DVF
 #filtres
 df = df_2020[(df_2020["Type local"] == "Appartement") | 
-             (df_2020["Type local"] == "Maison")|
+             (df_2020["Type local"] == "Maison")&
              (df_2020["Nature mutation"]=="Vente")]
-
+del df_2020
 #colonnes inutiles
 df.drop(["Code service CH","Reference document","1 Articles CGI",
               "2 Articles CGI","3 Articles CGI","4 Articles CGI",
@@ -23,21 +23,8 @@ df.drop(["Code service CH","Reference document","1 Articles CGI",
 df.drop_duplicates(subset = None, inplace = True)
 df = df.drop_duplicates(subset = ["Date mutation","Valeur fonciere","Code postal"], keep = False)
 
-#on enleve les prix qui sont aberrants
-outliers_max = df["Valeur fonciere"].quantile(0.95)
-outliers_min = df["Valeur fonciere"].quantile(0.01)
-df_clean = df[(df["Valeur fonciere"] < outliers_max) & (df["Valeur fonciere"] > outliers_min) ]
-
-#supression des surfaces aberrantes
-outliers_surface_max = df_clean["Surface reelle bati"].quantile(0.9999)
-outliers_surface_min = df_clean["Surface reelle bati"].quantile(0.001)
-df_clean = df_clean[(df_clean["Surface reelle bati"] < outliers_surface_max) & (df_clean["Surface reelle bati"] > outliers_surface_min)]
-
-#calcul du prix au m2
-df_clean["Prix m2"] = df_clean["Valeur fonciere"]/df_clean["Surface reelle bati"]
-
 #gestion des arrondissements de Paris, Lyon et Marseille
-df_clean["Commune2"] = df_clean["Commune"]
+df["Commune2"] = df["Commune"]
 dico = {"Commune2":{"PARIS 01":"PARIS","PARIS 02":"PARIS",
                               "PARIS 03":"PARIS",
                               "PARIS 04":"PARIS",
@@ -82,22 +69,65 @@ dico = {"Commune2":{"PARIS 01":"PARIS","PARIS 02":"PARIS",
                               "LYON 7EME":"LYON",
                               "LYON 8EME":"LYON",
                               "LYON 9EME":"LYON"}}
-df_clean = df_clean.replace(dico)
+df = df.replace(dico)
+del dico
 
-# prix au m2 aberrants
-outliers_prix = df_clean["Prix m2"].quantile(0.9999)
-df_clean = df_clean[df_clean["Prix m2"] < outliers_prix]
+df["Prix m2"] = df["Valeur fonciere"]/df["Surface reelle bati"]
+df["Paris"]= df["Commune2"].apply(lambda x : 1 if x == "PARIS" else 0)
 
-df_clean["Paris"]= df_clean["Commune2"].apply(lambda x : 1 if x == "PARIS" else 0)
+Paris = df[df["Paris"]==1]
+Autre_villes = df[df["Paris"]==0]
+
+#Gestion des valeurs abberantes Paris
+outliers_max_Paris = Paris["Valeur fonciere"].quantile(0.95)
+outliers_min_Paris = Paris["Valeur fonciere"].quantile(0.05)
+outliers_max_Paris_surface = Paris["Surface reelle bati"].quantile(0.98)
+outliers_min_Paris_surface = Paris["Surface reelle bati"].quantile(0.01)
+outliers_max_Paris_prix_m2= Paris["Prix m2"].quantile(0.98)
+outliers_min_Paris_prix_m2 = Paris["Prix m2"].quantile(0.1)
+
+Paris = Paris[(Paris["Valeur fonciere"] < outliers_max_Paris) & 
+              (Paris["Valeur fonciere"] > outliers_min_Paris) &
+              (Paris["Surface reelle bati"] < outliers_max_Paris_surface) &
+              (Paris["Surface reelle bati"] > outliers_min_Paris_surface) &
+              (Paris["Prix m2"] < outliers_max_Paris_prix_m2) &
+              (Paris["Prix m2"] > outliers_min_Paris_prix_m2) ]
+
+
+del outliers_max_Paris, outliers_min_Paris, outliers_max_Paris_surface, outliers_min_Paris_surface
+del    outliers_max_Paris_prix_m2, outliers_min_Paris_prix_m2
+
+#gestion des valeurs abberantes des villes autre que Paris
+outliers_max_Autre_villes = Autre_villes["Valeur fonciere"].quantile(0.98)
+outliers_min_Autre_villes = Autre_villes["Valeur fonciere"].quantile(0.1)
+outliers_max_Autre_villes_surface = Autre_villes["Surface reelle bati"].quantile(0.999)
+outliers_min_Autre_villes_surface = Autre_villes["Surface reelle bati"].quantile(0.0005)
+outliers_max_Autre_villes_prix_m2= Autre_villes["Prix m2"].quantile(0.998)
+outliers_min_Autre_villes_prix_m2 = Autre_villes["Prix m2"].quantile(0.125)
+
+Autre_villes = Autre_villes[(Autre_villes["Valeur fonciere"] < outliers_max_Autre_villes) & 
+              (Autre_villes["Valeur fonciere"] > outliers_min_Autre_villes) &
+              (Autre_villes["Surface reelle bati"] < outliers_max_Autre_villes_surface) &
+              (Autre_villes["Surface reelle bati"] > outliers_min_Autre_villes_surface) &
+              (Autre_villes["Prix m2"] < outliers_max_Autre_villes_prix_m2) &
+              (Autre_villes["Prix m2"] > outliers_min_Autre_villes_prix_m2) ]
+
+
+del outliers_max_Autre_villes, outliers_min_Autre_villes, outliers_max_Autre_villes_surface
+del outliers_min_Autre_villes_surface, outliers_max_Autre_villes_prix_m2, outliers_min_Autre_villes_prix_m2
+del Paris, Autre_villes
+
+df_clean = pd.concat([Paris,Autre_villes], axis = 0)
 
 ##Ajout des régions via le code département
 dep_reg = dep_reg.rename(columns = {"num_dep":"Code departement"})
-dico2 = {"Code departement":{"2A":"20","2B":"20"}}
+dico2 = {"Code departement":{"2A":"201","2B":"202"}}
 dep_reg = dep_reg.replace(dico2)
 dep_reg["Code departement"] = dep_reg["Code departement"].astype('int32')
 df_clean = df_clean.replace(dico2)
 df_clean["Code departement"] = df_clean["Code departement"].astype('int32')
 df_clean = df_clean.merge(right = dep_reg , on = 'Code departement', how = 'left')
+df_clean = df_clean[(df_clean["Code departement"] != 201) & (df_clean["Code departement"] != 202)]
 
 ## Ajout des communes possédant une surface maritime
 communes_littorales = communes_littorales.drop_duplicates(keep ="first")
@@ -109,5 +139,5 @@ dico3 = {"Mer":{"1":"OUI","0":"NON"}}
 df_clean = df_clean.replace(dico3)
 df_clean.drop("Classement", axis = 1, inplace = True)
 
-del communes_littorales, dep_reg, df, df_2020, dico, dico2, dico3, dummies
-del outliers_max, outliers_min, outliers_prix, outliers_surface_max, outliers_surface_min
+del communes_littorales, dep_reg, df, dico2, dico3, dummies
+
