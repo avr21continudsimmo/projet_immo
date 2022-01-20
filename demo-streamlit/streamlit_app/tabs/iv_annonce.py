f43116ea1b3bc73a743bf827ec86fc9c20014ad6
chvalois <32735527+chvalois@users.noreply.github.com>
2022-01-20 11:35:42 +0100

    reorganisation tabs streamlit

---
+++ demo-streamlit/streamlit_app/tabs/iv_annonce.py
@@ -0,0 +1,27 @@
+import matplotlib.image as img
+import streamlit as st
+
+title = "Annonce demo"
+sidebar_name = "4. Exemple d'annonce"
+
+def run():
+    
+    st.write("Nous avons sélectionné cette annonce comme exemple par défaut : ")
+    image = img.imread('../../models/demo.png')
+    st.image(image)
+
+    st.markdown("""
+                Nous déduisons de l'annonce les propriétés suivantes : 
+                - **Exposition Sud**
+                - **Lumineux**
+                - **Mezzanine**
+                - **Garage**
+                - **Terrasse**
+                - **Parking**
+                - **Proche écoles**
+                - **Proche commodités**
+                
+                D'autres propriétés pourraient être rajoutées après la visite du bien.""")   
+    
+        
+        
